84863886ffb7b29302efbe658d91c8478d6a9d9a
Ikedinobi <Ikdinobi@gmail.com>
2024-02-16 02:45:43 +0100

    3.1

--- src/FileOutputStreamz/FileOutputStreamExample2.java
+++ src/FileOutputStreamz/FileOutputStreamExample2.java
@@ -7,7 +7,7 @@ public class FileOutputStreamExample2 {
     public static void main(String[] args) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("testout2.txt");
-            String s = "Welcome to the Land of Milk and Honey";
+            String s = "Welcome to the Land of Milk and Honey.";
             byte[] sample = s.getBytes();  // converting the strings into bytes
             fileOutputStream.write(sample);
             fileOutputStream.close();
