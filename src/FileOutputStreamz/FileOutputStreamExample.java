6d37d0cdc58a19dbaccdadef7557f257a2aa283a
Ikedinobi <Ikdinobi@gmail.com>
2024-02-16 02:39:25 +0100

    3

--- src/FileOutputStreamz/FileOutputStreamExample.java
+++ src/FileOutputStreamz/FileOutputStreamExample.java
@@ -1,10 +1,12 @@
+package FileOutputStreamz;
+
 import java.io.FileOutputStream;
 
 public class FileOutputStreamExample {
     public static void main(String[] args) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream("test.txt");
-            fileOutputStream.write(50);
+            fileOutputStream.write(52);
             fileOutputStream.close();
         }catch (Exception e) {
             System.out.println(e);
---
+++ src/FileOutputStreamz/FileOutputStreamExample2.java
@@ -0,0 +1,20 @@
+package FileOutputStreamz;
+
+import java.io.FileOutputStream;
+
+public class FileOutputStreamExample2 {
+
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("testout2.txt");
+            String s = "Welcome to the Land of Milk and Honey";
+            byte[] sample = s.getBytes();  // converting the strings into bytes
+            fileOutputStream.write(sample);
+            fileOutputStream.close();
+            System.out.println("Success...");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
