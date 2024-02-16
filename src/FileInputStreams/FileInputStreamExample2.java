54bf1034c8bfae17dd55a2e091de44509e500aa0
Ikedinobi <Ikdinobi@gmail.com>
2024-02-16 03:20:47 +0100

    5

---
+++ src/FileInputStreams/FileInputStreamExample2.java
@@ -0,0 +1,21 @@
+package FileInputStreams;
+
+import java.io.FileInputStream;
+
+public class FileInputStreamExample2 {
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream("testout2.txt");
+            int i=0;
+            while((i = fileInputStream.read())!=-1){
+                System.out.print((char)i);
+            }
+            fileInputStream.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
